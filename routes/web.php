a12acf21089351c0e65907dd412f419a79e75ee8
Carlos Allen <allencarlosDev@gmail.com>
2021-08-03 13:50:57 -0400

    installing vue in laravel

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('/', function () {
 Route::get( 'chat', function () {
     return view( 'chat' );
 } );
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
