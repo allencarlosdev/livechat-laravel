b52e77df0f9f4573980d76abaa492bec7388de9e
Carlos Allen <allencarlosDev@gmail.com>
2021-08-05 00:06:20 -0400

    Changin Route (Chat)  to controller

--- app/Events/ChatEvent.php
+++ app/Events/ChatEvent.php
@@ -4,24 +4,27 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class ChatEvent
+class ChatEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    public $user;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct( $message, User $user )
     {
-        //
+        return $this->message = $message;
+        return $this->user = $user;
     }
 
     /**
@@ -31,6 +34,6 @@ class ChatEvent
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new PrivateChannel( 'chat' );
     }
 }
